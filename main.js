953cfc2483d7ff1d3e5eef5c7086ff61200acdbb
manish_tixdo <balyan05.manish@gmail.com>
2017-05-25 12:05:16 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var http =require('http')
+
+http.createServer(function(request,response){
+    response.writeHead(200,{'Content-Type':'text/plain'});
+    response.end('hello world\n');
+}).listen(8081);
+
+console.log('server running on localhost:8081');
